9471b3fd8a1054428184374cd9afcac4c5267bfb
Jason Pelletier <jpelletier19@me.com>
2016-08-28 16:37:26 -0500

    add method to model to run api request. working on js styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,6 @@ module ApplicationHelper
 
 end
 
-module QuoteHelper
 
-  def get_quote
-    quote_api = "http://quotes.rest/qod.json?category=inspire"
-    uri = URI(quote_api)
-    net = Net::HTTP.get_response(uri)
-    json = JSON.parse(net.body)
-  end
-
-end
 
 
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,10 @@
 class Quote < ApplicationRecord
+
+  def self.get_quote
+      quote_api = "http://quotes.rest/qod.json?category=inspire"
+      uri = URI(quote_api)
+      net = Net::HTTP.get_response(uri)
+      json = JSON.parse(net.body)
+  end
+
 end
