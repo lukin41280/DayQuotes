0c48730cbf5f0ce41f6f6b3043d2c9e56fd036bd
Jason Pelletier <jpelletier19@me.com>
2016-08-28 15:13:35 -0500

    able to get info from api, parse it, and display to view.  need to have layout print

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
 
-def get_quote
-  quote_api = "curl -v  -i -X GET http://quotes.rest/qod.json?category=inspire"
-  uri = URI(quote_api)
-  net = Net::HTTP.get_response(uri)
-  json = JSON.parse(net.body)
-end
+
 
 end
+
+module QuoteHelper
+
+  def get_quote
+    quote_api = "http://quotes.rest/qod.json?category=inspire"
+    uri = URI(quote_api)
+    net = Net::HTTP.get_response(uri)
+    json = JSON.parse(net.body)
+  end
+
+end
+
+
